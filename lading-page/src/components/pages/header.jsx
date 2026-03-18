a117f7056ad53a69dbe6d9fb3bdde53da36d3478
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-18 20:27:20 -0300

    atualizado

---
+++ lading-page/src/components/pages/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function header(){
+    return(
+        <header>
+            <div className="my_photo">
+                <h1>Reservado para fotografia de capa inicial</h1>
+            </div>
+        </header>
+    )
+}
