f707bc24f1d8afcd00e3516dd0f86e50a68e322e
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-24 13:55:03 -0300

    atualizado

--- lading-page/src/components/cover/cover.jsx
+++ lading-page/src/components/cover/cover.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Video from "../media/video_cover.mp4"
-import './cover.module.css'
+import style from './cover.module.css'
 
 export default function cover(){
     return(
-        <section className="cover">
+        <section className={style.cover}>
             <video src={Video} autoPlay muted playsInline loop></video>
         </section>
     )
