07e6120147c31fe744fdaabce9d7089b3802eacf
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-26 20:21:41 -0300

    adicionado imagens de trabalhos

--- lading-page/src/components/works/works.jsx
+++ lading-page/src/components/works/works.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import Orelha001 from "../media/imgs/piercing_orelha.png";
-import Orelha002 from "../media/imgs/piercing_orelha002.png";
+import Imagem001 from "../media/imgs/img_trabalho001.png";
+import Imagem002 from "../media/imgs/img_trabalho002.png";
+import Imagem003 from "../meida/imgs/img_trabalho003.png"
+import Imagem004 from "../meida/imgs/img_trabalho004.png"
+import Imagem005 from "../meida/imgs/img_trabalho005.png"
+import Imagem006 from "../meida/imgs/img_trabalho006.png"
 import ImageModal from "./ImageModal";
 import style from './works.module.css'
 
@@ -16,52 +20,42 @@ export default function works() {
         <div className={style.container_photos_inline}>
           <div className={style.container_photo}>
             <div className={style.photo}>
-              <ImageModal imageSrc={Orelha001} alt="Orelha001" />
+              <ImageModal imageSrc={Imagem001} alt="foto trabalho" />
             </div>
           </div>
 
           <div className={style.container_photo}>
             <div className={style.photo}>
-              <img src={Orelha002} alt="" />
+              <ImageModal imageSrc={Imagem002} alt="foto trabalho" />
             </div>
           </div>
 
           <div className={style.container_photo}>
-            <div className={style.photo}></div>
+            <div className={style.photo}>
+              <ImageModal imageSrc={Imagem003} alt="foto trabalho" />
+            </div>
           </div>
         </div>
 
          <div className={style.container_photos_inline}>
           <div className={style.container_photo}>
-            <div className={style.photo}></div>
+            <div className={style.photo}>
+              <ImageModal imageSrc={Imagem004} alt="foto trabalho" />
+            </div>
           </div>
 
           <div className={style.container_photo}>
-            <div className={style.photo}></div>
+            <div className={style.photo}>
+              <ImageModal imageSrc={Imagem005} alt="foto trabalho" />
+            </div>
           </div>
 
           <div className={style.container_photo}>
-            <div className={style.photo}></div>
+            <div className={style.photo}>
+              <ImageModal imageSrc={Imagem006} alt="foto trabalho" />
+            </div>
           </div>
         </div>
-
-         <div className={style.container_photos_inline}>
-          <div className={style.container_photo}>
-            <div className={style.photo}></div>
-          </div>
-
-          <div className={style.container_photo}>
-            <div className={style.photo}></div>
-          </div>
-
-          <div className={style.container_photo}>
-            <div className={style.photo}></div>
-          </div>
-        </div>
-
-        
-
-
       </div>
     </div>
   );
