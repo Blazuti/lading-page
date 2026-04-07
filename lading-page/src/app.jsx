fa84b59c1444d1804930215f7c217adde0cfc9fb
Blazuti <gustavo.blazuti@hotmail.com>
2026-04-06 23:00:17 -0300

    adicionado corousel bootstrep

--- lading-page/src/app.jsx
+++ lading-page/src/app.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Index from './pages/category'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 createRoot(document.getElementById('root')).render(
--- lading-page/src/components/works/works.jsx
+++ lading-page/src/components/works/works.jsx
@@ -5,55 +5,71 @@ import Imagem003 from "../media/imgs/img_trabalho003.png";
 import Imagem004 from "../media/imgs/img_trabalho004.png";
 import Imagem005 from "../media/imgs/img_trabalho005.png";
 import Imagem006 from "../media/imgs/img_trabalho006.png";
-import style from "./works.module.css";
+import "./custom.scss";
 
-const images = [Imagem001, Imagem002, Imagem003, Imagem004, Imagem005, Imagem006];
+
+const images = [
+  Imagem001,
+  Imagem002,
+  Imagem003,
+  Imagem004,
+  Imagem005,
+  Imagem006,
+];
 
 export default function Works() {
-
-
   return (
-    <div className={style.works}>
-<div className={style.container_works}>
-  
-        <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-indicators">
-            {images.map((_, index) => (
-              <button
-                key={index}
-                type="button"
-                data-bs-target="#carouselExample"
-                data-bs-slide-to={index}
-                className={index === 0 ? "active" : ""}
-                aria-label={`Slide ${index + 1}`}
-              ></button>
-            ))}
-          </div>
-          <div className="carousel-inner">
-            {images.map((img, index) => (
-              <div key={index} className={`carousel-item ${index === 0 ? "active" : ""}`}>
-                <img src={img} className="d-block w-100" alt={`Imagem dos trabalhos feitos ${index + 1}`} />
-              </div>
-            ))}
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-</div>
+    <div className="works">
+      <div
+        id="carouselExample"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide-to={index}
+              className={index === 0 ? "active" : ""}
+              aria-label={`Slide ${index + 1}`}
+            ></button>
+          ))}
+        </div>
+        <div className="carousel-inner">
+          {images.map((img, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <img
+                src={img}
+                className="d-block w-100"
+                alt={`Imagem dos trabalhos feitos ${index + 1}`}
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-
-
-
-
-
-
-
-
     </div>
   );
 }
